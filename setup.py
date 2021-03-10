96595bfaef645467b9094f7a99e1a3d0d661fa11
Victor Tavares <antonio.vneto@dataprev.gov.br>
2021-03-10 18:27:56 -0300

    starting project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Jupyter notebooks for manipulation and generate school records from students.',
+    author='Victor Tavares',
+    license='',
+)
